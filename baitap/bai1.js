f654afa3d39c9da80800c7ed8ddeca0db6c79ec5
p3t3r276 <p3t3r276@gmail.com>
2020-02-29 08:55:03 +0700

    bai tap 1-4

---
+++ baitap/bai1.js
@@ -0,0 +1,7 @@
+const x = 5;
+const y = 7;
+
+const sum = x + y;
+const abstract = x - y;
+const times = x * y;
+const divide = x / y;
---
+++ baitap/bai4.js
@@ -0,0 +1,5 @@
+const listAnimal = [
+  { name: "vit", age: 2 },
+  { name: "voi", age: 4 },
+  { name: "cho", age: 7 }
+];
